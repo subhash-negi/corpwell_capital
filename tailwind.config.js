c5952e6994574e0af2631c8e299426a1363d7b57
subhash-negi <81283313+subhash-negi@users.noreply.github.com>
2024-05-01 15:42:59 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    listStyleType:{
+      none:'none',
+      disc:'disc',
+      decimal:'decimal',
+      square:'square',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
